7cd3febae8f54ce523e4464c680fa5e2078cd84c
Thiago de Mello Bueno <thiago@madeofpeople.org>
2014-03-14 09:45:47 -0400

    a little masonry

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-console.log("!!!!");
 (function (kala, $, undefined) {
-
-	console.log(kala);
-
 	
+	kala.masonrySelectors = [
+		["#inspiration-thumbs", ".thumbnails"]
+	];
+
 	//Function to convert hex format to a rgb color
 	kala.math = {
 		hexDigits: new Array("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"),
@@ -16,17 +16,15 @@ console.log("!!!!");
 		}
 	};
 
-
-
 	kala.copyToClipboard = function (aString) {
 	  window.prompt("Copy to clipboard: Ctrl+C, Enter", aString);
 	};
 
 	var makeColorsCopyToClipBoard = function () {
 		$(".color-guide li").each(function () {
-			var colorElement = $(this);
-			var color = colorElement.css("background-color");
-			console.log(color);
+			var colorElement, color;
+			colorElement = $(this);
+			color = colorElement.css("background-color");
 			colorElement.on("click", function () {
 				kala.copyToClipboard(kala.math.rgbToHex(color));
 			});
@@ -37,10 +35,21 @@ console.log("!!!!");
 		$("body").scrollspy({ target: "#header" });
 	};
 
+	var initMasonry = function () {
+		console.log(kala.masonrySelectors);
+		$(kala.masonrySelectors).each(function () {
+			var el = $(this[0]);
+			var selector = this[1];
+			el.masonry({
+				itemSelector: selector
+			});
+		});
+	};
+
 	document.addEventListener("DOMContentLoaded", function () {
-		console.log("!!!!");
 		makeColorsCopyToClipBoard();
 		watchScroll();
+		initMasonry();
 	});
 
 })(window.kala = window.kala || {}, jQuery);
